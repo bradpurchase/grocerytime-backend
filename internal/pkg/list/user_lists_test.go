772e34271add25c1eb157cc50997caec66ff3237
Brad Purchase <bradpurchase@gmail.com>
2020-03-28 08:57:24 -0400

    more/better tests for RetrieveUserLists

--- internal/pkg/list/user_lists_test.go
+++ internal/pkg/list/user_lists_test.go
@@ -28,7 +28,7 @@ func TestRetrieveUserLists_NoLists(t *testing.T) {
 	assert.Equal(t, userLists, []models.List{})
 }
 
-func TestRetrieveUserLists_HasLists(t *testing.T) {
+func TestRetrieveUserLists_HasListsCreated(t *testing.T) {
 	dbMock, mock, err := sqlmock.New()
 	require.NoError(t, err)
 	db, err := gorm.Open("postgres", dbMock)
@@ -43,17 +43,57 @@ func TestRetrieveUserLists_HasLists(t *testing.T) {
 			"created_at",
 			"updated_at",
 		}).
-		AddRow(uuid.NewV4(), userID, "Test List", time.Now(), time.Now()).
-		AddRow(uuid.NewV4(), userID, "Test List 2", time.Now(), time.Now())
-	mock.ExpectQuery("^SELECT (.+) FROM \"lists\"*").
+		AddRow(uuid.NewV4(), userID, "Provigo Grocery List", time.Now(), time.Now()).
+		AddRow(uuid.NewV4(), userID, "Beer Store List", time.Now(), time.Now())
+	mock.
+		ExpectQuery("^SELECT lists.* FROM \"lists\"*").
 		WithArgs(userID).
 		WillReturnRows(listRows)
+
+	db.Exec("INSERT INTO lists (name, user_id) VALUES (?, ?)", "Not My List", uuid.NewV4())
 
 	userLists, err := RetrieveUserLists(db, userID)
 	require.NoError(t, err)
 	assert.Len(t, userLists, 2)
+	assert.Equal(t, userLists.([]models.List)[0].Name, "Provigo Grocery List")
+	assert.Equal(t, userLists.([]models.List)[0].UserID, userID)
+	assert.Equal(t, userLists.([]models.List)[1].Name, "Beer Store List")
+	assert.Equal(t, userLists.([]models.List)[1].UserID, userID)
 }
 
-//TODO more test cases needed:
-// - test that this doesn't return other user's lists
-// - test that this includes both lists created and joined
+func TestRetrieveUserLists_HasListsCreatedAndJoined(t *testing.T) {
+	dbMock, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	db, err := gorm.Open("postgres", dbMock)
+	require.NoError(t, err)
+
+	userID := uuid.NewV4()
+	sharingUserID := uuid.NewV4()
+	sharedListID := uuid.NewV4()
+	listRows := sqlmock.
+		NewRows([]string{
+			"id",
+			"user_id",
+			"name",
+			"created_at",
+			"updated_at",
+		}).
+		AddRow(uuid.NewV4(), userID, "Provigo Grocery List", time.Now(), time.Now()).
+		AddRow(uuid.NewV4(), userID, "Beer Store List", time.Now(), time.Now()).
+		AddRow(sharedListID, sharingUserID, "Shared List", time.Now(), time.Now())
+	mock.
+		ExpectQuery("^SELECT lists.* FROM \"lists\"*").
+		WithArgs(userID).
+		WillReturnRows(listRows)
+
+	db.Exec("INSERT INTO list_users (list_id, user_id) VALUES (?, ?)", sharedListID, userID)
+
+	userLists, err := RetrieveUserLists(db, userID)
+	require.NoError(t, err)
+	assert.Len(t, userLists, 3)
+	assert.Equal(t, userLists.([]models.List)[0].Name, "Provigo Grocery List")
+	assert.Equal(t, userLists.([]models.List)[0].UserID, userID)
+	assert.Equal(t, userLists.([]models.List)[1].Name, "Beer Store List")
+	assert.Equal(t, userLists.([]models.List)[2].Name, "Shared List")
+	assert.Equal(t, userLists.([]models.List)[2].UserID, sharingUserID)
+}
