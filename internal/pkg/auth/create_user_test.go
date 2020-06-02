6e736790229ed07957b9e46d58c2ef086665e0b8
Brad Purchase <bradpurchase@gmail.com>
2020-06-02 11:59:54 -0400

    Update create_user_test.go

--- internal/pkg/auth/create_user_test.go
+++ internal/pkg/auth/create_user_test.go
@@ -68,7 +68,7 @@ func TestCreateUser_UserCreated(t *testing.T) {
 		WithArgs(sqlmock.AnyArg(), listName, AnyTime{}, AnyTime{}).
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.NewV4()))
 	mock.ExpectQuery("^INSERT INTO \"list_users\" (.+)$").
-		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), "", true, true, AnyTime{}, AnyTime{}).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), true, true, AnyTime{}, AnyTime{}).
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.NewV4()))
 	mock.ExpectQuery("^INSERT INTO \"auth_tokens\" (.+)$").
 		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), AnyTime{}, AnyTime{}, AnyTime{}).
