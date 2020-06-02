70eaa3d88c294aede962610d1fee4ab0c2ea21cc
Brad Purchase <bradpurchase@gmail.com>
2020-06-02 16:54:14 -0400

    WIP handle item repositioning with collision detection

--- internal/pkg/grocerylist/update_item.go
+++ internal/pkg/grocerylist/update_item.go
@@ -25,12 +25,49 @@ func UpdateItem(db *gorm.DB, args map[string]interface{}) (interface{}, error) {
 		item.Quantity = args["quantity"].(int)
 	}
 	if args["position"] != nil {
-		item.Position = args["position"].(int)
+		newPosition := args["position"].(int)
+		currPosition := item.Position
+
+		// Handle item position collisions
+		if err := RepositionCollidingItem(db, item.ListID, currPosition, newPosition); err != nil {
+			return nil, err
+		}
+
+		item.Position = newPosition
 	}
 	if err := db.Save(&item).Error; err != nil {
 		return nil, err
 	}
 	return item, nil
+}
+
+// RepositionCollidingItem updates the position of a "colliding item" when performing
+// an item update on position.
+//
+// When a user updates the position of an item in a list, the new position may collide
+// with the position of an exising item. In this case, we need to update the colliding item's
+// position either upward or downward in the list into a new position.
+//
+//TODO note: we may need to do this recursively; i.e. call this function within itself
+// if the new position of the colliding item is also colliding
+func RepositionCollidingItem(db *gorm.DB, listID uuid.UUID, currPosition int, newPosition int) error {
+	collidingPosItem := &models.Item{}
+	if err := db.Where("list_id = ? AND position = ?", listID, newPosition).Find(&collidingPosItem).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return nil
+		}
+		return err
+	}
+	collidingItemNewPos := collidingPosItem.Position - 1
+	// If the repositioned item's current position is greater than its new position,
+	// move the colliding item's down the list by increasing its position by 1
+	if currPosition > newPosition {
+		collidingItemNewPos = collidingPosItem.Position + 1
+	}
+	if err := db.Model(&collidingPosItem).Where("id = ?", collidingPosItem.ID).Update("position", collidingItemNewPos).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 // GetNewPosition gets the new position of an updated item
