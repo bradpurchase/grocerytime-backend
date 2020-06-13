65bb7a49d7a434051c6bdf7e672031cf5acd6414
Brad Purchase <bradpurchase@gmail.com>
2020-06-13 12:42:32 -0400

    should fix build

--- internal/pkg/auth/create_user_test.go
+++ internal/pkg/auth/create_user_test.go
@@ -68,7 +68,7 @@ func TestCreateUser_UserCreated(t *testing.T) {
 		WithArgs(sqlmock.AnyArg(), listName, AnyTime{}, AnyTime{}).
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.NewV4()))
 	mock.ExpectQuery("^INSERT INTO \"list_users\" (.+)$").
-		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), true, AnyTime{}, AnyTime{}).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), "", true, AnyTime{}, AnyTime{}).
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(uuid.NewV4()))
 	mock.ExpectQuery("^INSERT INTO \"grocery_trips\" (.+)$").
 		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), AnyTime{}, AnyTime{}).
