4f16ef6ea1eb045c4a014ba9ba379752fa7c648d
Brad Purchase <bradpurchase@gmail.com>
2020-09-24 18:46:44 -0400

    replace SendNewUserEmail with new dynamic template version :fire:

--- internal/pkg/mailer/new_user.go
+++ internal/pkg/mailer/new_user.go
@@ -9,24 +9,23 @@ import (
 
 // SendNewUserEmail sends an email to a new user on signup
 func SendNewUserEmail(email string) (interface{}, error) {
+	m := mail.NewV3Mail()
 	from := mail.NewEmail("GroceryTime", "noreply@grocerytime.app")
-	subject := "Welcome to GroceryTime! 🛒"
-	to := mail.NewEmail("", email)
+	m.SetFrom(from)
+	m.SetTemplateID("d-323c957c7bdf42b095f96514e37db6e5")
 
-	plainTextContent := "Hello and welcome to GroceryTime, your grocery companion app! This is just a quick email to thank you for signing up."
-	htmlContent := "<p>Hello,</p>"
-	htmlContent += "<p>Welcome to GroceryTime, your grocery companion app! This is just a quick email to thank you for signing up and give you a few pointers on how to get started:</p>"
-	htmlContent += "<ol>"
-	htmlContent += "<li><strong>Create shopping lists:</strong> The first step after signing up is to tell us where you buy your groceries. This is so we can create a list for that store to get you started, but you can create as many as you'd like for each store you shop at!</li>"
-	htmlContent += "<li><strong>Organize your shopping:</strong> Let's say you do a weekly grocery run. You can easily keep your list organized with separate trips to stay organized. Just tap the checkmark icon at the top of the list when you're done shopping!</li>"
-	htmlContent += "<li><strong>Share with anyone:</strong> Most people who use GroceryTime use it to share grocery lists with their spouse or a friend. You can easily share any store list you've created and get work together - just use the share icon in the top right of the screen when viewing a store.</li>"
-	htmlContent += "</ol>"
-	htmlContent += "<p>Thanks again for signing up for a GroceryTime account. If you have any questions, concerns, or general feedback you can email us at <a href=\"mailto:support@grocerytime.app\">support@grocerytime.app</a>.</p>"
-	htmlContent += "<p>Cheers,<br />Brad from GroceryTime</p>"
-	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	p := mail.NewPersonalization()
+	toAddresses := []*mail.Email{
+		mail.NewEmail("", email),
+	}
+	p.AddTos(toAddresses...)
+	m.AddPersonalizations(p)
 
-	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
-	response, err := client.Send(message)
+	request := sendgrid.GetRequest(os.Getenv("SENDGRID_API_KEY"), "/v3/mail/send", "https://api.sendgrid.com")
+	request.Method = "POST"
+	var Body = mail.GetRequestBody(m)
+	request.Body = Body
+	response, err := sendgrid.API(request)
 	if err != nil {
 		return nil, err
 	}
