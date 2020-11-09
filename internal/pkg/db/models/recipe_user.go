5aa18177fea8017c02a3ee86552c7a2fb6cc64c5
Brad Purchase <bradpurchase@gmail.com>
2020-11-09 15:42:21 -0500

    meal_user should be recipe_user

--- internal/pkg/db/models/recipe_user.go
+++ internal/pkg/db/models/recipe_user.go
@@ -7,17 +7,17 @@ import (
 	"gorm.io/gorm"
 )
 
-// MealUser defines the model for meal_users, which represents a user's usage of a meal
-type MealUser struct {
-	ID     uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	MealID uuid.UUID `gorm:"type:uuid;not null"`
-	UserID uuid.UUID `gorm:"type:uuid;not null"`
+// RecipeUser defines the model for meal_users, which represents a user's usage of a recipe
+type RecipeUser struct {
+	ID       uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	RecipeID uuid.UUID `gorm:"type:uuid;not null"`
+	UserID   uuid.UUID `gorm:"type:uuid;not null"`
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
 
 	// Associations
-	Meal Meal
-	User User
+	Recipe Recipe
+	User   User
 }
