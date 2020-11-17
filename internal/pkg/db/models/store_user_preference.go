ff5d7722fe36d893fc55a5d5b3535b62a3a0fa49
Brad Purchase <bradpurchase@gmail.com>
2020-11-16 19:14:43 -0500

    when a store is marked as default, unmark default from any other stores for the user

--- internal/pkg/db/models/store_user_preference.go
+++ internal/pkg/db/models/store_user_preference.go
@@ -18,3 +18,37 @@ type StoreUserPreference struct {
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
 }
+
+// AfterUpdate hook handles some cleanup operations after updating store user prefs
+func (sup *StoreUserPreference) AfterUpdate(tx *gorm.DB) (err error) {
+	if sup.DefaultStore {
+		// Find all other stores belonging to this user and mark them as default_store false
+		var storeUsers []StoreUser
+		storeUsersQuery := tx.
+			Select("id").
+			Where("user_id IN (?)", tx.Model(&StoreUser{}).Select("user_id").Where("id = ?", sup.StoreUserID)).
+			Where("id NOT IN (?)", sup.StoreUserID).
+			Find(&storeUsers).
+			Error
+		if err := storeUsersQuery; err != nil {
+			return err
+		}
+
+		// Collect store user IDs and use them to update all other records to remove default flag
+		var storeUserIds []uuid.UUID
+		for i := range storeUsers {
+			storeUserIds = append(storeUserIds, storeUsers[i].ID)
+		}
+		if len(storeUserIds) > 0 {
+			updateQuery := tx.
+				Model(&StoreUserPreference{}).
+				Where("store_user_id IN (?)", storeUserIds).
+				Update("default_store", false).
+				Error
+			if err := updateQuery; err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
