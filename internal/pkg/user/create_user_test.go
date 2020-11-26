bad3d41fbfe8a8214f0e9cb35bdc39c2eaa11b2b
Brad Purchase <bradpurchase@gmail.com>
2020-11-26 13:26:29 -0500

    fix tests

--- internal/pkg/user/create_user_test.go
+++ internal/pkg/user/create_user_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (s *Suite) TestCreateUser_InvalidPassword() {
-	_, e := CreateUser("test@example.com", "", "John", uuid.NewV4())
+	_, e := CreateUser("test@example.com", "", "John", "iPhone", uuid.NewV4())
 	require.Error(s.T(), e)
 }
 
@@ -18,7 +18,7 @@ func (s *Suite) TestCreateUser_DuplicateEmail() {
 		WithArgs(email).
 		WillReturnRows(sqlmock.NewRows([]string{"count(1)"}).AddRow(1))
 
-	_, e := CreateUser(email, "password", "John", uuid.NewV4())
+	_, e := CreateUser(email, "password", "John", "iPhone", uuid.NewV4())
 	require.Error(s.T(), e)
 	assert.Equal(s.T(), e.Error(), "An account with this email address already exists")
 }
@@ -35,12 +35,16 @@ func (s *Suite) TestCreateUser_UserCreated() {
 		WithArgs(email, sqlmock.AnyArg(), name, nil, nil, AnyTime{}, AnyTime{}, AnyTime{}).
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(userID))
 
+	s.mock.ExpectExec("^DELETE FROM \"auth_tokens\"*").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	deviceName := "iGlasses"
 	clientID := uuid.NewV4()
 	s.mock.ExpectQuery("^INSERT INTO \"auth_tokens\" (.+)$").
-		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), AnyTime{}, AnyTime{}, AnyTime{}, sqlmock.AnyArg(), sqlmock.AnyArg()).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), AnyTime{}, sqlmock.AnyArg(), AnyTime{}, AnyTime{}, sqlmock.AnyArg(), sqlmock.AnyArg()).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "client_id", "user_id"}).AddRow(uuid.NewV4(), clientID, userID))
 
-	user, err := CreateUser(email, "password", name, clientID)
+	user, err := CreateUser(email, "password", name, deviceName, clientID)
 	require.NoError(s.T(), err)
 	assert.Equal(s.T(), user.Email, email)
 	assert.Equal(s.T(), user.Name, name)
