90e85db68f21a8d63c85f34f86f5f325878bf0f2
Brad Purchase <bradpurchase@gmail.com>
2020-12-17 19:38:46 -0500

    fix tests

--- internal/pkg/meals/create_recipe_test.go
+++ internal/pkg/meals/create_recipe_test.go
@@ -12,6 +12,9 @@ func (s *Suite) TestCreateRecipe_FullDetails() {
 	mealType := "Snack"
 	url := "https://www.food.com/recipe/traditional-peanut-butter-and-jelly-243965"
 	amount := 2.0
+	var unit string
+	unit1 := "tbsp"
+	unit2 := "tsp"
 	args := map[string]interface{}{
 		"name":     name,
 		"mealType": mealType,
@@ -25,13 +28,13 @@ func (s *Suite) TestCreateRecipe_FullDetails() {
 			map[string]interface{}{
 				"name":   "Peanut Butter",
 				"amount": amount,
-				"unit":   "tbsp",
+				"unit":   unit1,
 				"notes":  "spread evenly",
 			},
 			map[string]interface{}{
 				"name":   "Strawberry Jam",
 				"amount": amount,
-				"unit":   "tsp",
+				"unit":   unit2,
 				"notes":  "spread evenly",
 			},
 		},
@@ -45,11 +48,11 @@ func (s *Suite) TestCreateRecipe_FullDetails() {
 	assert.Equal(s.T(), len(recipe.Ingredients), 3)
 	assert.Equal(s.T(), recipe.Ingredients[0].Name, "Bread")
 	assert.Equal(s.T(), recipe.Ingredients[0].Amount, &amount)
-	assert.Equal(s.T(), recipe.Ingredients[0].Unit, nil)
+	assert.Equal(s.T(), recipe.Ingredients[0].Unit, &unit)
 	assert.Equal(s.T(), recipe.Ingredients[1].Name, "Peanut Butter")
 	assert.Equal(s.T(), recipe.Ingredients[1].Amount, &amount)
-	assert.Equal(s.T(), recipe.Ingredients[1].Unit, "tbsp")
+	assert.Equal(s.T(), recipe.Ingredients[1].Unit, &unit1)
 	assert.Equal(s.T(), recipe.Ingredients[2].Name, "Strawberry Jam")
 	assert.Equal(s.T(), recipe.Ingredients[2].Amount, &amount)
-	assert.Equal(s.T(), recipe.Ingredients[2].Unit, "tsp")
+	assert.Equal(s.T(), recipe.Ingredients[2].Unit, &unit2)
 }
