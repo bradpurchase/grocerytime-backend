a25f9bd20d016800049352af163e8b960f574a5b
Brad Purchase <bradpurchase@gmail.com>
2020-12-29 11:52:48 -0500

    WIP create meal (needs items)

--- internal/pkg/meals/plan_meal.go
+++ internal/pkg/meals/plan_meal.go
@@ -1,32 +1,43 @@
 package meals
 
 import (
-	"fmt"
+	"errors"
 
+	"github.com/bradpurchase/grocerytime-backend/internal/pkg/db"
 	"github.com/bradpurchase/grocerytime-backend/internal/pkg/db/models"
 	uuid "github.com/satori/go.uuid"
 )
 
 // PlanMeal creates a meal record and associated records
 func PlanMeal(userID uuid.UUID, args map[string]interface{}) (meal *models.Meal, err error) {
-	fmt.Println(args)
-	// recipeID := args["recipeID"].(uuid.UUID)
-	// name := args["name"].(string)
-	// var mealType string
-	// if args["mealType"] != nil {
-	// 	mealType = args["mealType"].(string)
-	// }
-	// var notes string
-	// if args["notes"] != nil {
-	// 	notes = args["notes"].(string)
-	// }
-	// date := args["date"].(time.Time)
-	// meal = &models.Meal{
-	// 	RecipeID: recipeID,
-	// 	Name:     name,
-	// 	MealType: &mealType,
-	// 	Notes:    &notes,
-	// 	Date:     date,
-	// }
+	var mealType string
+	if args["mealType"] != nil {
+		mealType = args["mealType"].(string)
+	}
+	var notes string
+	if args["notes"] != nil {
+		notes = args["notes"].(string)
+	}
+	date := args["date"].(string)
+	recipeID, err := uuid.FromString(args["recipeId"].(string))
+	if err != nil {
+		return meal, errors.New("recipeId arg not a UUID")
+	}
+
+	meal = &models.Meal{
+		RecipeID: recipeID,
+		UserID:   userID,
+		Name:     args["name"].(string),
+		MealType: &mealType,
+		Servings: args["servings"].(int),
+		Notes:    &notes,
+		Date:     date,
+	}
+	if err := db.Manager.Create(&meal).Error; err != nil {
+		return meal, err
+	}
+
+	// TODO: scan through args["items"] and add each item to args["storeID"]
+
 	return meal, nil
 }
