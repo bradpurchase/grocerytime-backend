86e6a6ec3a33ed34e67c90cd37c8ac0a45c9a8ce
Brad Purchase <bradpurchase@gmail.com>
2021-01-31 16:17:15 -0500

    cleanup migration
    remove uuid-oosp, other cleanups

--- internal/pkg/db/migration/main.go
+++ internal/pkg/db/migration/main.go
@@ -36,10 +36,9 @@ func AutoMigrateService(db *gorm.DB) error {
 	m.InitSchema(func(db *gorm.DB) error {
 		log.Println("[Migration.InitSchema] Initializing database schema...")
 
-		// Create the UUID extensions
+		// Create extensions
 		// postgres user needs to have superuser perms for now
 		db.Exec("CREATE EXTENSION \"pgcrypto\";")
-		db.Exec("CREATE EXTENSION \"uuid-oosp\";")
 
 		if err := migrate(db); err != nil {
 			return fmt.Errorf("[Migration.InitSchema]: %v", err)
@@ -69,7 +68,7 @@ func AutoMigrateService(db *gorm.DB) error {
 			// Add index idx_items_grocery_trip_id_category_id
 			ID: "202006021110_add_idx_items_grocery_trip_id_category_id",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.Exec("CREATE INDEX idx_grocery_trip_id_category_id ON items (grocery_trip_id, category_id)").Error
+				return tx.Exec("CREATE INDEX IF NOT EXISTS idx_grocery_trip_id_category_id ON items (grocery_trip_id, category_id)").Error
 			},
 			Rollback: func(tx *gorm.DB) error {
 				return tx.Exec("DROP INDEX idx_grocery_trip_id_category_id").Error
@@ -79,7 +78,7 @@ func AutoMigrateService(db *gorm.DB) error {
 			// Add index idx_store_users_store_id
 			ID: "202006021117_add_idx_store_users_store_id_user_id",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.Exec("CREATE INDEX idx_store_users_store_id_user_id ON store_users (store_id, user_id)").Error
+				return tx.Exec("CREATE INDEX IF NOT EXISTS idx_store_users_store_id_user_id ON store_users (store_id, user_id)").Error
 			},
 			Rollback: func(tx *gorm.DB) error {
 				return tx.Exec("DROP INDEX idx_store_users_store_id_user_id").Error
@@ -89,7 +88,7 @@ func AutoMigrateService(db *gorm.DB) error {
 			// Add index idx_items_grocery_trip_id_name
 			ID: "202010111143_add_idx_items_grocery_trip_id_name",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.Exec("CREATE INDEX idx_items_name_grocery_trip_id ON items (name, grocery_trip_id)").Error
+				return tx.Exec("CREATE INDEX IF NOT EXISTS idx_items_name_grocery_trip_id ON items (name, grocery_trip_id)").Error
 			},
 			Rollback: func(tx *gorm.DB) error {
 				return tx.Exec("DROP INDEX idx_items_name_grocery_trip_id").Error
@@ -99,16 +98,10 @@ func AutoMigrateService(db *gorm.DB) error {
 			// Add index for meals.date (string column)
 			ID: "202101091236_meals_date_index",
 			Migrate: func(tx *gorm.DB) error {
-				if err := tx.Exec("CREATE INDEX idx_meals_date ON meals (date)").Error; err != nil {
-					return err
-				}
-				return nil
+				return tx.Exec("CREATE INDEX IF NOT EXISTS idx_meals_date ON meals (date)").Error
 			},
 			Rollback: func(tx *gorm.DB) error {
-				if err := tx.Exec("DROP INDEX idx_meals_date").Error; err != nil {
-					return err
-				}
-				return nil
+				return tx.Exec("DROP INDEX idx_meals_date").Error
 			},
 		},
 		{
