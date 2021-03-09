a31bd6ce20f72e66cf5cebf0c93967c20d4af13f
Brad Purchase <bradpurchase@gmail.com>
2021-03-09 08:10:09 -0500

    add description and image_url columns to recipes table, add gql support

--- internal/pkg/db/models/recipe.go
+++ internal/pkg/db/models/recipe.go
@@ -9,11 +9,13 @@ import (
 
 // Recipe defines the model for recipes, which represents a global meal object
 type Recipe struct {
-	ID       uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	UserID   uuid.UUID `gorm:"type:uuid;not null;index:idx_recipes_user_id"`
-	Name     string    `gorm:"type:varchar(255);not null;index:idx_recipes_name"`
-	URL      *string   `gorm:"type:varchar(255)"`
-	MealType *string   `gorm:"type:varchar(10)"`
+	ID          uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	UserID      uuid.UUID `gorm:"type:uuid;not null;index:idx_recipes_user_id"`
+	Name        string    `gorm:"type:varchar(255);not null;index:idx_recipes_name"`
+	Description *string   `gorm:"type:text"`
+	MealType    *string   `gorm:"type:varchar(10)"`
+	URL         *string   `gorm:"type:varchar(255)"`
+	ImageURL    *string   `gorm:"type:text"`
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
